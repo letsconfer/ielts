1809efca3224c0f336448e929a47cf2cf4ff128e
Vikramjit Singh <111748657+letsconfer@users.noreply.github.com>
2026-09-22 12:40:43 +0530

    Update master-config.js

--- master-config.js
+++ master-config.js
@@ -15,5 +15,5 @@ window.CV_MASTER_CONFIG = {
     profileImage: "me.jpeg"     // Default profile image file
 
     // 4. Fixed homepage to language.html)
-    homePage: "language.html" // Points back to the main hub page under the vocab branch
+    homePage: "https://letsconfer.github.io/learn/Language.html" // Points back to the main hub page under the vocab branch
 };
